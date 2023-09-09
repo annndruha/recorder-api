5b4013489c817724389307e56e63b9162e1d8b26
Andrey Marakulin <annndruha.github@gmail.com>
2023-09-09 20:46:49 +0300

    Remove hot reload

--- sandbox/dynamic.py
+++ sandbox/dynamic.py
@@ -1,4 +1,4 @@
-# fastapi_dynamic_routes/dynamic_routes_includer.py
+# https://github.com/ashkangoleh/fastapi_dynamic_routers/blob/34559c57c4f37d708026e5e8627a7286574b831b/fastapi_dynamic_routers/dynamic_routes_includer.py
 from typing import Any
 from fastapi import FastAPI
 
@@ -19,12 +19,11 @@ class Routers:
             module = __import__(module_path, fromlist=[route_name])
             route_module = getattr(module, route_name)
             route_method_name = f'{route_name.title()}'
-            # print(route_method_name)
 
             def route_method():
                 return self.app.include_router(
                     route_module,
-                    prefix='/' + route_method_name.lower(),#self.prefix,
+                    prefix='/' + route_method_name.lower(),
                     tags=[route_method_name.title().replace('_', '')]
                 )
 
--- sandbox/static/custom_script.js
+++
@@ -1,2 +0,0 @@
-setTimeout(function(){console.log('Time to reload 3 sec')}, 2000);
-//setTimeout(function(){window.location.reload()}, 5000);
